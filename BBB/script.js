ad6bfbb2b360bdd10fa0c83c5fce552eb5d8f2ce
bravebehindbars <143886356+bravebehindbars@users.noreply.github.com>
2023-09-04 11:03:03 -0400

    Add files via upload

---
+++ BBB/script.js
@@ -0,0 +1,7 @@
+alert("Welcome to the Caged Grove Where Pink Oranges Grow!");
+function changeFontColor() {
+	var targetElement = document.getElementById("target");
+	var Color=prompt ("Select a Color"); 
+	targetElement.style.color = Color;
+}
+
